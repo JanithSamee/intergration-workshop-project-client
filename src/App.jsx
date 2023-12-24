6effadb0a5c81cdf7f6c7c5b80b07c3d41a3b9cb
janithsamee <janith.samee0@gmail.com>
2023-12-24 19:28:00 +0530

    added new components for day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Login from './pages/UserLogin';
-import Home from './pages/Home';
-import { MantineProvider } from '@mantine/core';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/UserLogin";
+import Home from "./pages/Home";
+import { MantineProvider } from "@mantine/core";
+import UserRegister from "./pages/UserRegister";
+import Post from "./pages/Post";
+import UserAccount from "./pages/UserAccount";
 
 export default function App() {
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Home/>,
-  },{
-    path:"/login",
-    element:<Login></Login>
-  }
-]);
-  return (
-    <MantineProvider>
-        <RouterProvider router={router} />
-    </MantineProvider>
-  )
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: <Login></Login>,
+        },
+        {
+            path: "/register",
+            element: <UserRegister></UserRegister>,
+        },
+        {
+            path: "/posts",
+            element: <Post></Post>,
+        },
+        {
+            path: "/account",
+            element: <UserAccount></UserAccount>,
+        },
+    ]);
+    return (
+        <MantineProvider>
+            <RouterProvider router={router} />
+        </MantineProvider>
+    );
 }
-
